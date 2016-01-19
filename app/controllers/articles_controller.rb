a22d8bb3a5dafa818510c4df201322c1497786fe
Spencer <spencer.mortenson@gmail.com>
2016-01-19 03:23:09 +0000

    Commit for deploy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :show, :update, :destroy]
 
   def new
     @article = Article.new
@@ -15,17 +16,14 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(params[:id])
 
   end
   #this is my attempt at the editing page
   def edit
-    @article = Article.find(params[:id])
 
   end
 
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
       flash[:notice]="article was succesfully updates"
       redirect_to article_path(@article)
@@ -39,12 +37,15 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     flash[:notice] = "Article was successfully deleted"
     redirect_to articles_path
   end
   private
+  def set_article
+    @article = Article.find(params[:id])
+
+  end
   def article_params
     params.require(:article).permit(:title, :description)
   end
