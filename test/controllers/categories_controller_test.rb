4f63db105818f09ddceda4cfe2d0b428b48d09f2
Spencer <spencer.mortenson@gmail.com>
2016-06-01 15:04:40 +0000

    Categories only admin feature

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -4,6 +4,7 @@ class CategoriesControllerTest < ActionController::TestCase
 
   def setup
     @category = Category.create(name: "sports")
+    @user = User.create(username: "john", email: "john@example.com", password: "password", admin: true)
   end
 
   test "should get categories index" do
@@ -13,15 +14,22 @@ class CategoriesControllerTest < ActionController::TestCase
   end
 
   test "should get new" do
+    session[:user_id] = @user.id
     get :new
     assert_response :success
 
   end
 
   test "should get show" do
+    
     get(:show, {"id" => @category.id})
     assert_response :success
   end
-
+test "Should redirect create when admin not logged in" do
+  assert_no_difference 'Category.count' do
+    post :create, catgeory: {name: "sports" }
+  end
+  assert_redirected_to categories_path
+end
 
 end
